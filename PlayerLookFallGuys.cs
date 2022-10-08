3e78b7261240ad2a2833437be1febbe90b22da55
korgix <korpoineminikitos@rambler.ru>
2022-10-08 21:12:07 +0500

    Create PlayerLookFallGuys.cs

---
+++ PlayerLookFallGuys.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerLookFallGuys : MonoBehaviour
+{
+    [SerializeField] float mouseSense = 1;
+
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        float rotateX = Input.GetAxis("Mouse X") * mouseSense;
+        float rotateY = Input.GetAxis("Mouse Y") * mouseSense;
+        Vector3 rotPlayer = transform.rotation.eulerAngles;
+
+        rotPlayer.y += rotateX;
+
+        transform.rotation = Quaternion.Euler(rotPlayer);
+    }
+}
