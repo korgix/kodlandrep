4f6f4cf4d7b29db445b1ac8ecb8a0dc47f915117
korgix <korpoineminikitos@rambler.ru>
2022-10-08 21:11:36 +0500

    Create Bounce.cs

---
+++ Bounce.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    [SerializeField] int force;
+
+    private void Start()
+    {
+        force = Random.Range(50, 80);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<Rigidbody>().AddForce(new Vector3(0, force, 0), ForceMode.Impulse);
+        }
+    }
+}
